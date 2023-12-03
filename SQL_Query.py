14298dc88f4fe8e83cf6228b537ea96db5230e61
zkfn8 <zkfn8@umsystem.edu>
2023-12-02 20:11:42 -0600

    describe tables function

--- SQL_Query.py
+++ SQL_Query.py
@@ -9,19 +9,36 @@ db = mysql.connector.connect(
 
 mycursor = db.cursor()
 
+def Describe_Database():
+    mycursor.execute("SHOW tables")
+    for x in mycursor:
+        print(x)
+    
+    
+def Add_Employees(fnam, mname, lname, password, authorization):
+    mycursor.execute()
 
+
+def Add_Swarm():
+    mycursor.execute("")
+
+
+def Add_Chompskis():
+    mycursor.execute("")
+    
 def Get_Authorization(employee_id : int):
-    authorization = mycursor.execute("SELECT authorization FROM Employee WHERE employee_id = " + employee_id)
+    authorization = mycursor.execute(
+        "SELECT authorization FROM Employee WHERE employee_id = {}".format(employee_id))
     return authorization 
 
 def Describe_Tables(Table : str):
-    mycursor.execute("DESCRIBE " + Table)
+    mycursor.execute("DESCRIBE {}".format(Table))
     for x in mycursor:
         print(x)
     
     
 def Create_Tables():
-    mycursor.execute("CREATE TABLE Employee (employee_id int PRIMARY KEY AUTO_INCREMENT,fname VARCHAR(50) NOT NULL, mname VARCHAR(50),lname VARCHAR(50) NOT NULL, password VARCHAR(50) NOT NULL, authorization smallint NOT NULL)")
+    mycursor.execute("CREATE TABLE Employee (employee_id int PRIMARY KEY AUTO_INCREMENT,fname VARCHAR(50) NOT NULL, mname VARCHAR(50),lname VARCHAR(50) NOT NULL, password VARCHAR(50) NOT NULL, authorization smallint NOT NULL CHECK(authorization = '0' or authorization = '1' or authorization = '2'))")
     mycursor.execute("CREATE TABLE Swarm (swarm_id int PRIMARY KEY AUTO_INCREMENT, quantity int, latitude double(9, 5), longitude double (9,5))")
     mycursor.execute("CREATE TABLE Oversees (employee_id int, swarm_id int, FOREIGN KEY(employee_id) REFERENCES Employee(employee_id),  FOREIGN KEY(swarm_id) REFERENCES Swarm(swarm_id))")
     mycursor.execute("CREATE TABLE Gnome_Chompskis (chompskis_id int PRIMARY KEY, age smallint, name VARCHAR(50), height double(10,2), weight double (10,2), no_teeth int UNSIGNED, swarm_id int, FOREIGN KEY(swarm_id) REFERENCES Swarm(swarm_id))")
--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import SQL_Query as sq
 
-def main():
     
+def Describe_Tables():
+    sq.Describe_Database()
+    table = input("Which table would you like to see?: ")
+    sq.Describe_Tables(table)
+    
+    
+def Increase_Biodiversity(employee_id):
+    if sq.get_Authorization(employee_id) == 2:
+        while new != "s" or new != "c":
+            new = input("Do you want to add a new swarm or Chompski? [s/c]")
+            if new == "s":
+                sq.Add_Swarm()
+            elif new == "c":
+                sq.Add_Chompski
+    else:
+        print("you do not have acces to this function")
+        
+def main():
+    Describe_Tables()
     quit()
 
 if __name__ == "__main__":
     main()
-    
-    
-def Increase_Biodiversity(employee_id):
-    if sq.Get_Authorization(employee_id) == 2:
-        while new_swarm != "s" or new_swarm != "c":
-            new_swarm = input("Do you want to add a new swarm or Chompski?")
-    else:
-        print("you do not have acces to this function")
